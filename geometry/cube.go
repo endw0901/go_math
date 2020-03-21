1c232caba6d9f064388313f97d1b4f7f19105a8e
endw0901 <endw0901@gmail.com>
2020-03-22 04:38:55 +0900

    zero edge is not allowed

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,6 +1,23 @@
 package geometry
 
 // cubeの体積計算
-func CubeVolume(n int) int {
-	return n * n * n
+func CubeVolume(n int) (int, error) int {
+	if n!=0{
+		return n * n * n
+	}else{
+		return 0, errors.New("Zero length edge is not allowed")
+	}
+	
 }
+
+// vMAJOR.MINOR.PATCH
+/*
+Semantic Versioning = ユニバーサルなバージョニング仕様
+x.y.z
+x:major
+y:minor
+z:patch version number
+v1.2.3
+major1,minor2,patch3
+
+*/
